63f4c0577bc461e6b5aeab58aeb1cb6c0c459948
Jared Ackley <jrod3323@gmail.com>
2021-01-02 14:54:50 -0600

    add express api route and html route requirements, add addioitnal dependencies

---
+++ server.js
@@ -0,0 +1,33 @@
+// NPM Requirements
+
+var express = require("express");
+
+// Create Express server
+const express = require("express");
+const { v4: uuidv4 } = require('uuid');
+const fs = require("fs");
+const dbJSON = require("./db.json");
+const path = require("path");
+
+// Sets an initial port
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ================================================================================
+// Route Handling
+// ================================================================================
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
